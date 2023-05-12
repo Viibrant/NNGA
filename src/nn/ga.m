1224fa14f10caae35f12eefdc11e934088cb3a9d
UP953058 <UP953068@myport.ac.uk>
2023-05-12 15:11:48 +0100

    added ga file

---
+++ src/nn/ga.m
@@ -0,0 +1,22 @@
+function y = Shubert1(x)
+    n = length(x);
+    sum1 = 0;
+    for i = 1:n
+      for j = 1:5
+        sum1 = sum1 + j * cos((j + 1) * x(i) + j);
+      end
+    end
+    y = sum1;
+  end
+
+options = optimoptions('ga', 'PopulationSize', 100, 'MutationFcn',
+  'mutationadaptfeasible','CrossoverFcn', 'crossoverscattered',
+  'EliteCount', 2, 'SelectionFcn', 'selectionstochunif', 'PlotFcn',
+  @gaplotbestf);
+
+nvars = 2; % Number of variables
+lb = [-10, -10]; % Lower bounds
+ub = [10, 10]; % Upper bounds
+[x,fval] = ga(@Shubert1, nvars, [], [], [], [], lb, ub, [], options);
+
+options = optimoptions('ga', 'PlotFcn', @gaplotbestf);
